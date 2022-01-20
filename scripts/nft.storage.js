6b277e112d4871a288fac7a2bc9cda6f53798583
Chris Blanquera <chris@incept.asia>
2022-01-21 02:06:32 +0800

    nft.storage upload script

--- scripts/nft.storage.js
+++ scripts/nft.storage.js
@@ -1,96 +1,84 @@
+// $ node scripts/nft.storage.js
+
 const fs = require('fs')
 const path = require('path')
 
 const { NFTStorage, File, Blob } = require('nft.storage')
 require('dotenv').config()
 
-async function main() {
-  const logo = path.resolve(__dirname, '../contract/logo.png')
-  const contract = path.resolve(__dirname, '../contract/contract.json')
-  const images = path.resolve(__dirname, '../build/images')
-  const jsons = path.resolve(__dirname, '../build/json')
-  if (!fs.existsSync(images)) {
-    throw new Error('build/images folder missing')
+const collections = require('../data/collections.json')
+const client = new NFTStorage({ token: process.env.NFT_STORAGE })
+
+async function uploadCollection(folder, collection) {
+  const preview = path.resolve(__dirname, '../data', folder, 'preview.jpg')
+  const videos = path.resolve(__dirname, '../data', folder, 'videos')
+  const jsons = path.resolve(__dirname, '../data', folder, 'json')
+  if (!fs.existsSync(videos)) {
+    throw new Error(`data/${folder}/videos folder missing`)
   } else if (!fs.existsSync(jsons)) {
-    throw new Error('build/json folder missing')
-  } else if (!fs.existsSync(logo)) {
-    throw new Error('contract/logo.png missing')
-  } else if (!fs.existsSync(contract)) {
-    throw new Error('contract/contract.json missing')
+    throw new Error(`data/${folder}/json folder missing`)
+  } else if (!fs.existsSync(preview)) {
+    throw new Error(`data/${folder}/preview.jpg missing`)
   }
+
+  //upload preview
+  console.log(`Uploading ${folder}/preview.jpg ...`)
+  const previewCID = await client.storeBlob(new Blob([await fs.promises.readFile(preview)]))
+  console.log(`${folder}/preview.jpg is now https://ipfs.io/ipfs/${previewCID}`)
 
   const jsonFolder = []
-  const client = new NFTStorage({ token: process.env.NFT_STORAGE })
 
-  //add contract.json
-  const contractdata = require(contract);
-  if (contractdata.image.indexOf('TODO') !== -1) {
-    console.log('Uploading logo.png ...')
-    const logoCID = await client.storeBlob(new Blob([
-      await fs.promises.readFile(logo)
-    ]))
-    console.log(`logo.png is now https://ipfs.io/ipfs/${logoCID}`)
-    
-    contractdata.image = `https://ipfs.io/ipfs/${logoCID}`
-    console.log('Updating contract.json ...')
-    fs.writeFileSync(contract, JSON.stringify(contractdata, null, 2));
-  }
-  
-  jsonFolder.push(
-    new File([await fs.promises.readFile(contract)], path.basename(contract))
-  )
-
-  //loop through image files
-  const files = await fs.promises.readdir(images);
-  for( const name of files ) {
-    const image = path.join(images, name)
-    const imageStat = await fs.promises.stat( image )
-    //if not a file
-    if(!imageStat.isFile()) {
-      console.error(`Skipping ${name}, not a file`)
-      continue
-    //if not a png
-    } else if (path.extname(image) !== '.png') {
-      console.error(`Skipping ${name}, not a png`)
-      continue
+  for (let i = 0; i < collection.limit; i++) {
+    const index = i + 1
+    const video = path.join(videos, `${index}.mp4`)
+    if (!fs.existsSync(video)) {
+      console.error(`Skipping: ${folder}/videos/${path.basename(video)} missing.`)
+      continue;
     }
-
-    const json = path.join(jsons, `${path.basename(name, '.png')}.json`)
-    //if file doesnt exist
+    const json = path.join(jsons, `${i}.json`)
     if (!fs.existsSync(json)) {
-      console.error(`Skipping ${name}, no matching ${path.basename(name, '.png')}.json found`)
-      continue
-    }
-    const jsonStat = await fs.promises.stat( json )
-    //if no matching json
-    if(!jsonStat.isFile()) {
-      console.error(`Skipping ${name}, no matching ${path.basename(name, '.png')}.json found`)
-      continue
-    }
-
-    const jsondata = require(json)
-    if (jsondata.image.indexOf('TODO') !== -1) {
-      //upload image
-      console.log(`Uploading ${name} ...`)
-      const data = await fs.promises.readFile(image)
+      //upload video
+      console.log(`Uploading ${folder}/videos/${path.basename(video)} ...`)
+      const data = await fs.promises.readFile(video)
       const cid = await client.storeBlob(new Blob([data]))
-      console.log(`${name} is now https://ipfs.io/ipfs/${cid}`)
+      console.log(`${folder}/videos/${path.basename(video)} is now https://ipfs.io/ipfs/${cid}`)
       //update json file
-      jsondata.image = `https://ipfs.io/ipfs/${cid}`
-      console.log(`Updating ${path.basename(json)} ...`)
-      fs.writeFileSync(json, JSON.stringify(jsondata, null, 2))
-    } else {
-      console.log(`${name} already uploaded to ${jsondata.image}`)
-    }
+      const jsondata = Object.assign({}, collection.metadata)
+      jsondata.image = `https://ipfs.io/ipfs/${previewCID}`
+      jsondata.animation_url = `https://ipfs.io/ipfs/${cid}`
 
+      jsondata.name = json.name.replace('{ID}', index)
+      jsondata.description = json.description.replace('{ID}', index)
+      jsondata.external_url = json.external_url.replace('{ID}', index)
+
+      console.log(`Updating ${folder}/json/${path.basename(json)} ...`)
+      fs.writeFileSync(json, JSON.stringify(jsondata, null, 2))
+    }
+    
     jsonFolder.push(
       new File([await fs.promises.readFile(json)], path.basename(json))
     )
   }
 
   console.log('Uploading all json...')
-  const cid = await client.storeDirectory(jsonFolder)
-  console.log(`JSON folder found in https://ipfs.io/ipfs/${cid}`)
+  const jsonCID = await client.storeDirectory(jsonFolder)
+  console.log(`JSON for ${folder} folder found in https://ipfs.io/ipfs/${jsonCID}`)
+}
+
+function truncateJson() {
+  for (const folder in collections) {
+    fs.rmdirSync(
+      path.resolve(__dirname, '../data', folder, 'json'), 
+      { recursive: true }
+    );
+  }
+}
+
+async function main() {
+  //truncateJson() //uncomment if starting over
+  for (const folder in collections) {
+    await uploadCollection(folder, collections[folder])
+  }
 
   console.log('Done!')
 }
